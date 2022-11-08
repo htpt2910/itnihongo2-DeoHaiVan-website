5a63abe1ec179e4efe8f627adfd0beb117a10913
htpt2910 <hmc2910@gmail.com>
2022-11-08 17:31:44 +0700

    Fix swagger ui not show

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,24 +3,26 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import { Footer } from './components/user/Footer'
 import { Navbar } from './components/user/Navbar'
 import Dashboard from './pages/admin/Dashboard'
+import { Logout } from './pages/admin/Logout'
 import { Posts } from './pages/admin/Posts'
 import { User } from './pages/admin/User'
-import { About } from './pages/user/About'
 import { Home } from './pages/user/Home'
 
 function App() {
   return (
-    <Router>
-      <Navbar />
-      <Routes>
-        <Route path="/about" element={<About />} />
-        <Route path="/user" element={<User />} />
-        <Route path="/post" element={<Posts />} />
-        <Route path="/admin" exact="true" element={<Dashboard />} />
-        <Route path="/" exact="true" element={<Home />} />
-      </Routes>
-      <Footer />
-    </Router>
+    <>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" exact="true" element={<Home />} />
+          <Route path="/user" element={<User />} />
+          <Route path="/post" element={<Posts />} />
+          <Route path="/logout" element={<Logout />} />
+          <Route path="/admin" exact="true" element={<Dashboard />} />
+        </Routes>
+        <Footer />
+      </Router>
+    </>
   )
 }
 
