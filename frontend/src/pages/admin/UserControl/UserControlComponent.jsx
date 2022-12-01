cd49753223a7bcfccea2139ba61770de9e9701f3
ThongThuyDung <thongthuydung123@gmail.com>
2022-12-01 19:44:33 +0700

    backup user management

--- frontend/src/pages/admin/UserControl/UserControlComponent.jsx
+++ frontend/src/pages/admin/UserControl/UserControlComponent.jsx
@@ -1,7 +1,7 @@
 import {
-  DeleteTwoTone, EditTwoTone, SearchOutlined
+  DeleteTwoTone,EyeTwoTone, EditTwoTone, SearchOutlined
 } from '@ant-design/icons';
-import { Space, Table } from 'antd';
+import {Modal, Popconfirm, Select, Space, Table} from "antd";
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import "./styles.css";
@@ -9,7 +9,10 @@ import "./styles.css";
 const UserControlComponent = () => {
   const [filteredInfo, setFilteredInfo] = useState({});
   const [sortedInfo, setSortedInfo] = useState({});
-  const [users, setUsers] = useState([])
+  const [users, setUsers] = useState([]);
+  const [data, setData] = useState([]);
+  const [modalData, setModalData] = useState([]);
+  const [isModalVisible, setIsModalVisible] = useState(false);
 
   const handleChange = (pagination, filters, sorter) => {
     console.log('Various parameters', pagination, filters, sorter);
@@ -17,6 +20,7 @@ const UserControlComponent = () => {
     setSortedInfo(sorter);
   };
 
+  var current_user
   const columns = [
     {
       title: 'id',
@@ -50,6 +54,30 @@ const UserControlComponent = () => {
       width: "45%"
     },
     {
+      title: "View",
+      dataIndex: "view",
+      key: "view",
+      render: (_, record) => {
+        return (
+          <div>
+            <p
+              style={{color: "#3383FF"}}
+              onClick={ async () => {
+                console.log("record id: ", record.id)
+                current_user = users.filter((user) => user.id === record.id )
+                console.log("current user: ", current_user)
+                setModalData(current_user);
+                setIsModalVisible(true);
+              }}
+              >
+                View
+              </p>
+          </div>
+        );
+      },
+      width: "7%",
+    },
+    {
       title: 'Action',
       key: 'action',
       render: (_, record) => (
@@ -72,7 +100,10 @@ const UserControlComponent = () => {
           [...prevValue, {
             id: user.id,
             username: user.username,
-            email: user.email
+            email: user.email,
+            gender: user.gender,
+            age: user.age,
+            image: user.image
           }]
         ))
         
@@ -93,8 +124,34 @@ const UserControlComponent = () => {
         <div className="tableContainer">
           <Table columns={columns} dataSource={users} onChange={handleChange} className="table" />
         </div>
-      </div>
-    </>
+      <Modal
+          mask={false}
+          title="Profile User Preview"
+          open={isModalVisible}
+          onCancel={() => setIsModalVisible(false)}
+          width="50%"
+          height="70%"
+          okButtonProps={{style: {display: "none"}}}
+          bodyStyle={{
+            overflowY: "auto",
+            maxHeight: "calc(100vh - 250px)",
+          }}
+        >
+          <div style={{ textAlign: 'center' }}>
+          <p>Username: {modalData[0].username}</p>
+            <img
+              src={modalData[0].image}
+              alt="img"
+              style={{width: "50%", height: "50%", margin: "20px 0px"}}
+            /> 
+            <p>Email: {modalData[0].email}</p>
+            <p>Gender: {modalData[0].gender ? "Nam" : "Nữ"}</p>
+            <p>Age: {modalData[0].age}</p>
+            
+          </div>
+        </Modal>
+    </div>
+  </>
   );
 };
 export default UserControlComponent;
